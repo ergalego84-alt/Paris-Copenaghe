262bf9299f907e6e6050fba5336b32dbe212d212
ergalego84-alt <ergalego84@gmail.com>
2026-09-07 22:46:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,15 @@
-const CACHE_NAME = 'paris-cph-v5';
+const CACHE_NAME = 'paris-cph-v6';
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
   event.waitUntil(self.clients.claim());
 });
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(
+    fetch(event.request, { cache: 'no-store' }).catch(() => caches.match(event.request))
+  );
 });
